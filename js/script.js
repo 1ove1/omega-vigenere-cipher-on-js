e744adf0e0e9bb8ff9469f30e658094bb34c0ded
AndreyBulavskiy <10vvvvvv31@gmail.com>
2021-10-19 13:15:01 +0300

    init

---
+++ js/script.js
@@ -0,0 +1,15 @@
+const text = document.getElementById('input-area__text');
+const key = document.getElementById('input-area__key');
+const result = document.getElementById('input-area__result');
+
+function do_some_work() {
+
+	
+
+}
+
+function deb() {
+	console.log(text.value);
+	console.log(key.value);
+	console.log(result.value);
+}
